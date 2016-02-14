1b49c0ea60ea6d4e6eeaa6dabd1a52479c4f98f2
Andrey <andreimitev93@gmail.com>
2016-02-14 11:00:38 +0200

    Initial Commit. Installed autofac, Glimpse, removed uneccessary libraries.

---
+++ Data/Mastilo.Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace Mastilo.Data.Models
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ Web/Mastilo.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mastilo.Web.Startup))]
+namespace Mastilo.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
